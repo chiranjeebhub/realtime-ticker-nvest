4d860a3b71f34f31ecdd5255504f611ff07aead1
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-07-10 19:31:26 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import RTChart from "react-rt-chart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    setInterval(() => this.forceUpdate(), 1000);
+  }
+  getBTC = () => {};
+  render() {
+    var data = {
+      date: new Date(),
+      BTC: Math.random(),
+      ETH: Math.random()
+    };
+    return (
+      <div className="App">
+        <h1>Nvest BTC & ETH Ticker</h1>
+        <RTChart fields={["BTC", "ETH"]} data={data} />
+      </div>
+    );
+  }
 }
 
 export default App;
